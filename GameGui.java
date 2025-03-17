a79c23300ffeee5b95df5d40febb91f6b8312894
Hammy2015 <hammy2015@gmail.com>
2025-03-17 00:35:28 -0400

    Finished Dice, updated some stuff in GameGui regarding spelling mistakes/missing semicolon, started basic Match.java stuff

--- GameGui.java
+++ GameGui.java
@@ -4,24 +4,12 @@ import java.awt.*;
 public class GameGui{
     JFrame main;
 
-    public gui(){
+    public GameGui(){
         main = new JFrame("My First GUI");
         main.setSize(400, 300);
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // Create a button
-        JButton button = new JButton("Click Me!");
-        
-        // Create a label
-        JLabel label = new JLabel("Hello, GUI!");
 
-        // Get the content pane of the frame
-        Container contentPane = main.getContentPane();
-        contentPane.setLayout(new FlowLayout()); // Use a layout manager
-
-        // Add components to the content pane
-        contentPane.add(button);
-        contentPane.add(label);
+        main.add(main);
 
         main.setVisible(true);
 
