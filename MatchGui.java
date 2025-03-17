8a94fd30c131912a6334d6e4d279c33da81e8cb5
Jacob Hamilton <hammy2015@gmail.com>
2025-03-17 13:32:22 -0400

    Started working on Match Gui

--- MatchGui.java
+++ MatchGui.java
@@ -2,7 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public Class MatchGui{
+    private int score;
+    private int enemyScore;
+
     public MatchGui(){
-        
+        match JFrame = new JFrame("Farckle Match");
+
+        match.setSize(900,600);
+
+        ArrayList<> labels = new ArrayList<JLabel>();
+        ArrayList<> buttons = new ArrayList<JButton>();
+        ArrayList<> buttons = new ArrayList<JButton>();
+        for(int i = 0; i < 6; i++){
+            labels.add(new JLabel("Dice #" + (i + 1)))
+            labels.add(new JButton("Dice #" + (i + 1)))
+        }
     }
 }
