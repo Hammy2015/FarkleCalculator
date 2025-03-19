14ddb42c20c0a2700cfa5bcb643f1f5a9cde99f2
Hammy2015 <hammy2015@gmail.com>
2025-03-18 22:00:43 -0400

    MatchGui now has arrays rather than individual fields

--- MatchGui.java
+++ MatchGui.java
@@ -6,20 +6,10 @@ import java.util.ArrayList;
 public class MatchGui extends JPanel{
     private int score;
     private int enemyScore;
-    private JRadioButton diceOneRadio;
-    private JRadioButton diceTwoRadio;
-    private JRadioButton diceThreeRadio;
-    private JRadioButton diceFourRadio;
-    private JRadioButton diceFiveRadio;
-    private JRadioButton diceSixRadio;
+    private JRadioButton[] diceRadio;
+    private JTextField[] diceField;
     private JLabel explanationLabel;
-    private JTextField diceOneField;
-    private JTextField diceTwoField;
-    private JTextField diceThreeField;
-    private JTextField diceFourField;
-    private JTextField diceFiveField;
-    private JTextField diceSixField;
-    private JToggleButton rollButton;
+    private JButton rollButton;
     private JLabel scoreLabel;
     private JTextField scoreField;
 
@@ -30,79 +20,52 @@ public class MatchGui extends JPanel{
 
         ArrayList<JLabel> labels = new ArrayList<>();
         ArrayList<JButton> buttons = new ArrayList<>();
-        for(int i = 0; i < 6; i++){
-            labels.add(new JLabel("Dice #" + (i + 1)));
-            buttons.add(new JButton("Roll"){
-                public void actionPerformed(ActionEvent e){
-                    // Roll the dice
-                }
-            });
+        diceRadio = new JRadioButton[6];
+        diceField = new JTextField[6];
+        for (int i = 0; i < 6; i++){
+            diceRadio[i] = new JRadioButton("Dice # " + (i + 1));
+            diceField[i] = new JTextField(5);
+            diceField[i] = new JTextField(5);
+            diceField[i].setEnabled(false);
+            add(diceRadio[i]);
+            add(diceField[i]);
         }
-        diceOneRadio = new JRadioButton ("Dice # 1");
-        diceTwoRadio = new JRadioButton ("Dice # 2");
-        diceThreeRadio = new JRadioButton ("Dice # 3");
-        diceFourRadio = new JRadioButton ("Dice # 4");
-        diceFiveRadio = new JRadioButton ("Dice # 5");
-        diceSixRadio = new JRadioButton ("Dice # 6");
         explanationLabel = new JLabel ("Hold dice");
-        diceOneField = new JTextField (5);
-        diceTwoField = new JTextField (5);
-        diceThreeField = new JTextField (5);
-        diceFourField = new JTextField (5);
-        diceFiveField = new JTextField (5);
-        diceSixField = new JTextField (5);
-        rollButton = new JToggleButton ("Roll", false);
+        rollButton = new JButton ("Roll"){
+            public void actionPerformed(ActionEvent e){
+                // Roll the dice
+            }
+        };
         scoreLabel = new JLabel ("My Score");
         scoreField = new JTextField (5);
-
-        //set components properties
-        diceOneField.setEnabled (false);
-        diceTwoField.setEnabled (false);
-        diceThreeField.setEnabled (false);
-        diceFourField.setEnabled (false);
-        diceFiveField.setEnabled (false);
-        diceSixField.setEnabled (false);
-        scoreField.setEnabled (false);
-
         //adjust size and set layout
         setPreferredSize (new Dimension (225, 313));
         setLayout (null);
 
         //add components
-        add (diceOneRadio);
-        add (diceTwoRadio);
-        add (diceThreeRadio);
-        add (diceFourRadio);
-        add (diceFiveRadio);
-        add (diceSixRadio);
         add (explanationLabel);
-        add (diceOneField);
-        add (diceTwoField);
-        add (diceThreeField);
-        add (diceFourField);
-        add (diceFiveField);
-        add (diceSixField);
         add (rollButton);
         add (scoreLabel);
         add (scoreField);
 
         //set component bounds (only needed by Absolute Positioning)
-        diceOneRadio.setBounds (120, 95, 95, 25);
-        diceTwoRadio.setBounds (120, 120, 100, 25);
-        diceThreeRadio.setBounds (120, 145, 100, 25);
-        diceFourRadio.setBounds (120, 170, 100, 25);
-        diceFiveRadio.setBounds (120, 195, 100, 25);
-        diceSixRadio.setBounds (120, 220, 100, 25);
+        diceRadio[0].setBounds (120, 95, 95, 25);
+        diceRadio[1].setBounds (120, 120, 100, 25);
+        diceRadio[2].setBounds (120, 145, 100, 25);
+        diceRadio[3].setBounds (120, 170, 100, 25);
+        diceRadio[4].setBounds (120, 195, 100, 25);
+        diceRadio[5].setBounds (120, 220, 100, 25);
         explanationLabel.setBounds (120, 70, 100, 25);
-        diceOneField.setBounds (20, 95, 100, 25);
-        diceTwoField.setBounds (20, 120, 100, 25);
-        diceThreeField.setBounds (20, 145, 100, 25);
-        diceFourField.setBounds (20, 170, 100, 25);
-        diceFiveField.setBounds (20, 195, 100, 25);
-        diceSixField.setBounds (20, 220, 100, 25);
+        diceField[0].setBounds (20, 95, 100, 25);
+        diceField[1].setBounds (20, 120, 100, 25);
+        diceField[2].setBounds (20, 145, 100, 25);
+        diceField[3].setBounds (20, 170, 100, 25);
+        diceField[4].setBounds (20, 195, 100, 25);
+        diceField[5].setBounds (20, 220, 100, 25);
         rollButton.setBounds (65, 250, 100, 25);
         scoreLabel.setBounds (20, 0, 100, 25);
         scoreField.setBounds (20, 20, 100, 25);
-
     }
+
+
 }
